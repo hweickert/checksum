154e7b1d878a9c004a854c2f3ec73ba9f3ecefe5
hweickert <henryweickert@gmail.com>
2015-09-02 00:37:18 +0200

    Update setup.py
    Preparing for pypi

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ import setuptools
 setuptools.setup(
     name = "checksum",
     packages = ["checksum"],
-    version="1.0.0"
+    version="1.0.0",
+    description="Creates checksums for files and directories.",
+    author = 'Henry Weickert',
+    author_email = 'henryweickert@gmail.com',
+    url = 'https://github.com/hweickert/checksum',
+    keywords = ['checksum', 'md5', 'sha1', 'checksumdir']
 )
